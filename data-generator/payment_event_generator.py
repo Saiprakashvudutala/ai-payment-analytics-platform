9fbc84687a31c865b92dfbdfb2b3debcd6ac7b3d
Sai Prakash Vudutala <saiprakash0905@gmail.com>
2026-06-10 18:32:47 +0530

    Build payment transaction generator

---
+++ data-generator/payment_event_generator.py
@@ -0,0 +1,58 @@
+from faker import Faker
+import random
+import pandas as pd
+from datetime import datetime
+
+fake = Faker()
+
+MERCHANTS = [
+    "Amazon",
+    "Flipkart",
+    "Swiggy",
+    "Zomato",
+    "Myntra",
+    "BigBasket",
+    "IRCTC"
+]
+
+CITIES = [
+    "Hyderabad",
+    "Bangalore",
+    "Mumbai",
+    "Pune",
+    "Chennai",
+    "Delhi"
+]
+
+STATUS = [
+    "SUCCESS",
+    "FAILED",
+    "PENDING"
+]
+
+
+def generate_transaction():
+
+    return {
+        "txn_id": f"TXN{random.randint(100000,999999)}",
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "sender_id": f"USR{random.randint(1000,9999)}",
+        "receiver_id": f"USR{random.randint(1000,9999)}",
+        "amount": round(random.uniform(10, 50000), 2),
+        "merchant": random.choice(MERCHANTS),
+        "city": random.choice(CITIES),
+        "status": random.choice(STATUS)
+    }
+
+
+transactions = []
+
+for _ in range(1000):
+    transactions.append(generate_transaction())
+
+df = pd.DataFrame(transactions)
+
+df.to_csv("transactions.csv", index=False)
+
+print("Generated 1000 transactions successfully!")
+print(df.head())
